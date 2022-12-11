5717f57550d81931c2ea8d3884de2704840216cd
Theonewhomadethings <88815725+Theonewhomadethings@users.noreply.github.com>
2022-12-11 13:46:36 +0000

    Update plotComparison.py

--- FunctionFromTable/plotComparison.py
+++ FunctionFromTable/plotComparison.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-originalData = np.genfromtxt("formatData.txt")
-print(originalData)
+originalData = np.loadtxt("Formatdata.txt")
 plt.plot(originalData[:,0], originalData[:,1], 'r-')
 plt.xlabel("Photon Energy [ev] ")
 plt.ylabel("Photons / sec ")
+plt.title("Photon energy spectrum from synchotron radiation source.")
 #plt.legend()
-plt.savefig("pythonPlot.png")
+plt.savefig("pythonPlot1.png")
